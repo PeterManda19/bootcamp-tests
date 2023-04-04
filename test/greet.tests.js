3edf752ecf5978a1c91bcb992f7202a7da741151
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-04 16:04:43 +0200

    testinf functionality of mocha

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,26 +1,35 @@
-// Import the assert module from Node.js
-import { strictEqual } from 'assert';
+// // Import the assert module from Node.js
+// import { strictEqual } from 'assert';
 
-// Import the greet function
-import greet from './path/to/greet.js';
+// // Import the greet function
+// import greet from './path/to/greet.js';
 
-// Describe the tests for the greet function
-describe('greet', function() {
-  // Test case 1: should return a greeting with the given name
-  it('should return a greeting with the given name', function() {
-    const result = greet('Alice');
-    strictEqual(result, 'Hello, Alice');
+// // Describe the tests for the greet function
+// describe('greet', function() {
+//   // Test case 1: should return a greeting with the given name
+//   it('should return a greeting with the given name', function() {
+//     const result = greet('Alice');
+//     strictEqual(result, 'Hello, Alice');
+//   });
+
+//   // Test case 2: should return a greeting with an empty string if no name is provided
+//   it('should return a greeting with an empty string if no name is provided', function() {
+//     const result = greet('');
+//     strictEqual(result, 'Hello, ');
+//   });
+
+//   // Test case 3: should return a greeting with the provided name, even if it contains leading or trailing whitespace
+//   it('should return a greeting with the provided name, even if it contains leading or trailing whitespace', function() {
+//     const result = greet('  Bob  ');
+//     strictEqual(result, 'Hello,   Bob  ');
+//   });
+// });
+
+describe('this test' , function(){
+  it('should show you how to use MochaJS' , function(){
+      assert.equal(2,2);
+
+      assert.deepEqual([2,2],[2,2]);
   });
 
-  // Test case 2: should return a greeting with an empty string if no name is provided
-  it('should return a greeting with an empty string if no name is provided', function() {
-    const result = greet('');
-    strictEqual(result, 'Hello, ');
-  });
-
-  // Test case 3: should return a greeting with the provided name, even if it contains leading or trailing whitespace
-  it('should return a greeting with the provided name, even if it contains leading or trailing whitespace', function() {
-    const result = greet('  Bob  ');
-    strictEqual(result, 'Hello,   Bob  ');
-  });
 });
