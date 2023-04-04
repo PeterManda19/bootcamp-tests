003f3d847591b1b2125c7d445c989a8b0a309e78
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-04 15:54:58 +0200

    greet function TDD

---
+++ test/greet.tests.js
@@ -0,0 +1,26 @@
+// Import the assert module from Node.js
+import { strictEqual } from 'assert';
+
+// Import the greet function
+import greet from './path/to/greet.js';
+
+// Describe the tests for the greet function
+describe('greet', function() {
+  // Test case 1: should return a greeting with the given name
+  it('should return a greeting with the given name', function() {
+    const result = greet('Alice');
+    strictEqual(result, 'Hello, Alice');
+  });
+
+  // Test case 2: should return a greeting with an empty string if no name is provided
+  it('should return a greeting with an empty string if no name is provided', function() {
+    const result = greet('');
+    strictEqual(result, 'Hello, ');
+  });
+
+  // Test case 3: should return a greeting with the provided name, even if it contains leading or trailing whitespace
+  it('should return a greeting with the provided name, even if it contains leading or trailing whitespace', function() {
+    const result = greet('  Bob  ');
+    strictEqual(result, 'Hello,   Bob  ');
+  });
+});
