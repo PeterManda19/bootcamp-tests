4fc8dac0113a3ce2eee7ccbcffc2a24eab12b139
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-18 15:37:19 +0200

    isFromBellville test fixes

--- isFromBellville.js
+++ isFromBellville.js
@@ -1,4 +1,14 @@
-function isFromBellville(vrnumber){
-    return vrnumber.startsWith("CY");
-  };
+function isFromBellville(vrnumber) {
+  // Check if vrnumber is a string
+  if (typeof vrnumber !== "string") {
+    return false;
+  }
+
+  // Convert vrnumber to uppercase to handle input of varying case
+  vrnumber = vrnumber.toUpperCase();
+
+  // Check if vrnumber starts with 'CY'
+  return vrnumber.startsWith("CY");
+}
+
   
