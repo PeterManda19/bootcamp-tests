7041acf4160c722d07d3c0688f302b48c21ec091
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-21 14:06:12 +0200

    fixing failing tests

--- isWeekday.js
+++ isWeekday.js
@@ -1,4 +1,4 @@
 function isWeekday(dayName){
-    const trimmedDayName = dayName.trim().toLowerCase();
+    const trimmedDayName = dayName.trim();
     return trimmedDayName.endsWith("day") && (!trimmedDayName.startsWith("s"));
 }
