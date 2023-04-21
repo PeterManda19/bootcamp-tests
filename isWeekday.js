f24fe5fe7eafe73b801d4cce701574a80cc1b413
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-21 19:42:11 +0200

    isWeekday function

--- isWeekday.js
+++ isWeekday.js
@@ -1,4 +1,5 @@
-function isWeekday(dayName){
+function isWeekday(dayName) {
     const trimmedDayName = dayName.trim();
-    return trimmedDayName.endsWith("day") && (!trimmedDayName.startsWith("S")) && (!trimmedDayName[0].toLowerCase());
+    return trimmedDayName.endsWith("day") && trimmedDayName[0].toUpperCase() !== "S";
 }
+  
