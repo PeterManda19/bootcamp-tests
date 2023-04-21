c9ad98fe2f336c3cf4dd4ce1fb67260922b7472a
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-21 12:53:59 +0200

    fixing isWeekDay assertion errors

--- isWeekday.js
+++ isWeekday.js
@@ -1,3 +1,4 @@
 function isWeekday(dayName){
-    return dayName.endsWith("day") && (!dayName.startsWith("S"));
+    const trimmedDayName = dayName.trim().toLowerCase();
+    return trimmedDayName.endsWith("day") && (!trimmedDayName.startsWith("s"));
 }
